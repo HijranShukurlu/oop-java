b99dc2d126d427313c90da9b2f2ccec77789f7e8
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-08 13:04:34 +0400

    Update Main.java

--- lesson3/Main.java
+++ lesson3/Main.java
@@ -1,4 +1,17 @@
-public class Main{
+public class Main {
+    public static void main(String[] args) {
+        Pet dog = new Pet("dog", "Rock", 5, 75, new String[]{"eat", "drink", "sleep"});
+        Human mother = new Human("Jane", "Karleone", 1950);
+        Human father = new Human("Vito", "Karleone", 1945);
+        Human child = new Human("Michael", "Karleone", 1977, 90, dog, mother, father, null);
 
+        System.out.println(child);
+        child.greetPet();
+        child.describePet();
+        dog.eat();
+        dog.respond();
+        dog.foul();
+        child.feedPet(false);
+    }
 }
 
