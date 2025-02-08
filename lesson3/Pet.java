afc93abb2d6b53e379fec4c33d3dc7ad0a33d651
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-08 13:05:15 +0400

    Update Pet.java

--- lesson3/Pet.java
+++ lesson3/Pet.java
@@ -1,33 +1,41 @@
-public class Pet {
+import java.util.Arrays;
+
+class Pet {
     String species;
     String nickname;
     int age;
     int trickLevel;
     String[] habits;
-    public Pet(String species,String nickname){
-        this.species=species;
-        this.nickname=nickname;
-    }
-    public Pet(String species,String nickname,int age,int trickLevel,String[] habits){
-        this.species=species;
-        this.nickname=nickname;
-        this.age=age;
-        this.trickLevel=trickLevel;
-        this.habits=habits;
-    }
-    public Pet(){}
 
-    public void eat(){
-        System.out.println("I am eating.");
-    }
-    public void respond(){
-        System.out.printf("Hello,owner.I am %s.I miss you.",this.nickname);
-    }
-    public void foul(){
-        System.out.println("I need to cover it up.");
-    }
-    @Override public void toString(){
-        System.out.printf("%s{nickname='%s',age=%d,trickLevel=%s,habits=[eat,drink,sleep]}",this.species,this.nickname,this.trickLevel);
+    public Pet() {}
+
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
     }
 
+    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
+    }
+
+    void eat() {
+        System.out.println("I am eating");
+    }
+
+    void respond() {
+        System.out.println("Hello, owner. I am " + nickname + ". I miss you!");
+    }
+
+    void foul() {
+        System.out.println("I need to cover it up");
+    }
+
+    @Override
+    public String toString() {
+        return species + "{nickname='" + nickname + "', age=" + age + ", trickLevel=" + trickLevel + ", habits=" + Arrays.toString(habits) + "}";
+    }
 }
