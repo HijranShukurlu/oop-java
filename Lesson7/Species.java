508972c4726f743e49cd79843ca9f1f0c5c3d7b3
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-21 23:42:17 +0400

    Create Species.java

---
+++ Lesson7/Species.java
@@ -0,0 +1,16 @@
+public enum Species {
+    DOG(true, 4, true),
+    CAT(false, 4, true),
+    PARROT(true, 2, false),
+    FISH(false, 0, false);
+
+    private final boolean canFly;
+    private final int numberOfLegs;
+    private final boolean hasFur;
+
+    Species(boolean canFly, int numberOfLegs, boolean hasFur) {
+        this.canFly = canFly;
+        this.numberOfLegs = numberOfLegs;
+        this.hasFur = hasFur;
+    }
+}
