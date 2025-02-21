b21ee849ab93e10cfa986fc81113b91dcc24ceae
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-21 23:22:29 +0400

    Update Human.java

--- Lesson7/Human.java
+++ Lesson7/Human.java
@@ -1,1 +1,18 @@
+class Human {
+    private String name;
+    private String surname;
+    private int year;
+    private String[][] schedule;
+    private Family family;
 
+    public Human(String name, String surname, int year, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.schedule = schedule;
+    }
+
+    public void finalize() {
+        System.out.println("Human object is being removed: " + name + " " + surname);
+    }
+}
