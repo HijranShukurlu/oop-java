bd21a9c8872d470505030aad2a5ca6da01e95d1a
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-21 23:22:04 +0400

    Update Pet.java

--- Lesson7/Pet.java
+++ Lesson7/Pet.java
@@ -1,1 +1,19 @@
+class Pet {
+    private Species species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
 
+    public Pet(Species species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
+    }
+
+    public void finalize() {
+        System.out.println("Pet object is being removed: " + nickname);
+    }
+}
