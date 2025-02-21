0185c21c4be62f97680627f66b4125d735ea1d42
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-21 23:45:34 +0400

    Update Pet.java

--- Lesson7/Pet.java
+++ Lesson7/Pet.java
@@ -1,22 +1,13 @@
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Pet {
-    private String species;
+    private Species species;
     private String nickname;
     private int age;
     private int trickLevel;
     private String[] habits;
 
-    static {
-        System.out.println("Loading class: Pet");
-    }
-
-    {
-        System.out.println("Creating a new Pet object");
-    }
-
-    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+    public Pet(Species species, String nickname, int age, int trickLevel, String[] habits) {
         this.species = species;
         this.nickname = nickname;
         this.age = age;
@@ -24,13 +15,7 @@ public class Pet {
         this.habits = habits;
     }
 
-    public void eat() {
-        System.out.println("I am eating.");
-    }
-    public void respond() {
-        System.out.printf("Hello, owner. I am %s. I miss you.%n", this.nickname);
-    }
-    public void foul() {
-        System.out.println("I need to cover it up.");
+    public void finalize() {
+        System.out.println("Pet " + nickname + " is being deleted");
     }
 }
