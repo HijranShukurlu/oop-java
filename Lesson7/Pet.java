04f231961deca95917764bec071682230d66ebae
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-02-22 23:52:50 +0400

    Update Pet.java

--- Lesson7/Pet.java
+++ Lesson7/Pet.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Pet {
     private Species species;
@@ -6,6 +7,11 @@ public class Pet {
     private int age;
     private int trickLevel;
     private String[] habits;
+
+    public Pet(Species species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
 
     public Pet(Species species, String nickname, int age, int trickLevel, String[] habits) {
         this.species = species;
@@ -15,7 +21,80 @@ public class Pet {
         this.habits = habits;
     }
 
-    public void finalize() {
-        System.out.println("Pet " + nickname + " is being deleted");
+    public Pet() {}
+
+    public void eat() {
+        System.out.println("I am eating.");
+    }
+
+    public void respond() {
+        System.out.printf("Hello, owner. I am " + this.nickname + ". I miss you.");
+    }
+
+    public void foul() {
+        System.out.println("I need to cover it up.");
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    @Override
+    public String toString() {
+        return species + "{nickname=" + nickname + ", age=" + age + ", trickLevel=" + trickLevel + ", habits=" + Arrays.toString(habits) + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pet pet)) return false;
+        return age == pet.age && trickLevel == pet.trickLevel && species == pet.species && Objects.equals(nickname, pet.nickname) && Arrays.equals(habits, pet.habits);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(species, nickname, age, trickLevel, Arrays.hashCode(habits));
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Pet object is being garbage collected: " + this);
+        super.finalize();
     }
 }
