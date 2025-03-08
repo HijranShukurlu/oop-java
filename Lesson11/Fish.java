a72ad5f4aa2714966786018f20172b93f681e9b0
HijranShukurlu <hijran.shukurlu.std@bhos.edu.az>
2025-03-08 21:59:12 +0400

    Create Fish.java

---
+++ Lesson11/Fish.java
@@ -0,0 +1,10 @@
+public class Fish extends Pet {
+    public Fish(String nickname, int age, int trickLevel, String[] habits) {
+        super(Species.FISH, nickname, age, trickLevel, habits);
+    }
+
+    @Override
+    public void respond() {
+        System.out.println(getNickname() + " is swimming around.");
+    }
+}
